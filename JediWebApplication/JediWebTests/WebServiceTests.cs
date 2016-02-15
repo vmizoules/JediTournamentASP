d3ed1142817db69b833bba97053918f05bc77c14
Rabérin Alexandre <kernelith@live.fr>
2016-02-15 18:37:38 +0100

    Déplacement de la solution initiale dans un sous dossier
    Ajoute de la solution qui contiendra le site (ASP.NET) et du projet de tests unitaire du web service

---
+++ JediWebApplication/JediWebTests/WebServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JediWebApplication.JediWebService;
+
+namespace JediWebApplication
+{
+    [TestClass]
+    public class WebServiceTests
+    {
+        [TestMethod]
+        public void TestGetAllJedis()
+        {
+            JediWebServiceClient client = new JediWebServiceClient();
+            // TODO
+        }
+    }
+}
