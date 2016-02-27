ba119985b5c19781d4bcc2bb2f0c9e6d2c4f87c1
Vincent <dark-vince15@hotmail.fr>
2016-02-27 10:39:32 +0100

    Add controllers and links in homepage & layout

---
+++ JediWebSiteApplication/JediWebSiteApplication/Controllers/TournoiController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JediWebSiteApplication.Controllers
+{
+    public class TournoiController : Controller
+    {
+        //
+        // GET: /Tournoi/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Tournoi/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Tournoi/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tournoi/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Tournoi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tournoi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Tournoi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tournoi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
