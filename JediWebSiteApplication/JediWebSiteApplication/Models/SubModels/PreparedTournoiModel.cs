f228b470228b7c1ef636c6f2ac4e800759bdfad0
Rabérin Alexandre <kernelith@live.fr>
2016-03-11 15:48:45 +0100

    Commentaires + play des match un peu plus correct

--- JediWebSiteApplication/JediWebSiteApplication/Models/SubModels/PreparedTournoiModel.cs
+++ JediWebSiteApplication/JediWebSiteApplication/Models/SubModels/PreparedTournoiModel.cs
@@ -7,9 +7,19 @@ namespace JediWebSiteApplication.Models.SubModels
 {
     public class PreparedTournoiModel
     {
+        /// <summary>
+        /// Tournoi à exécuter.
+        /// </summary>
         private TournoiModel m_tournoi;
+        /// <summary>
+        /// Match calculés pour le tournoi.
+        /// </summary>
         private List<MatchModel> m_matchs;
 
+        /// <summary>
+        /// Constructeur.
+        /// </summary>
+        /// <param name="tournoi">Tournoi à traiter.</param>
         public PreparedTournoiModel(TournoiModel tournoi)
         {
             m_tournoi = tournoi;
@@ -25,9 +35,8 @@ namespace JediWebSiteApplication.Models.SubModels
             // Calcul des 2 matchs (Demi finale)
             for (int i = 0 ; i < 2; i++)
             {
-                MatchModel mm = new MatchModel();
+                MatchModel mm = affectMatchMainData(m_matchs[2 * i], m_matchs[2 * i + 1], null);
                 mm.PhaseTournoi = EPhaseTournoiModel.DemiFinale;
-                affectMatchMainData(mm, m_matchs[2 * i], m_matchs[2 * i + 1], null);
                 // Fait jouer le match
                 computeMatch(mm);
 
@@ -35,17 +44,25 @@ namespace JediWebSiteApplication.Models.SubModels
             }
 
             // Calcul du dernier match (Finale)
-            MatchModel mFinale = new MatchModel();
+            MatchModel mFinale = affectMatchMainData(m_matchs[4], m_matchs[5], null);
             mFinale.PhaseTournoi = EPhaseTournoiModel.Finale;
-            affectMatchMainData(mFinale, m_matchs[4], m_matchs[5], null);
             // Fait jouer le match
             computeMatch(mFinale);
 
             m_matchs.Add(mFinale);
         }
 
-        private void affectMatchMainData(MatchModel m, MatchModel p1, MatchModel p2, StadeModel s)
+        /// <summary>
+        /// Affecte les valeurs principales pour remplir les données d'un match à partir de ses précédents.
+        /// </summary>
+        /// <param name="p1">Premier match précédent.</param>
+        /// <param name="p2">Second match précédent.</param>
+        /// <param name="s">Stade dans lequel se déroule le match.</param>
+        /// <returns>Match créer à partir des paramètres.</returns>
+        private MatchModel affectMatchMainData(MatchModel p1, MatchModel p2, StadeModel s)
         {
+            MatchModel m = new MatchModel();
+
             // Affecte le jedi 1 gagant du précédent match
             int idWinner1 = p1.IdVainqueur;
             m.Jedi1 = idWinner1 == p1.Jedi1.ID ? p1.Jedi1 : p1.Jedi2;
@@ -58,13 +75,28 @@ namespace JediWebSiteApplication.Models.SubModels
             m.Stade = s; // Non utile pour l'affichage donc non affecté
 
             m.ID = -2;
+
+            return m;
         }
 
+        /// <summary>
+        /// Calcul le résultat d'un match.
+        /// </summary>
+        /// <param name="match">Match à calculer.</param>
         public void computeMatch(MatchModel match)
         {
-            match.IdVainqueur = match.Jedi1.ID; // TODO real compute
+            Random r = new Random();
+
+            // Affecte le vainqueur
+            if (/* Jedi 1*/ r.Next(100) > r.Next(100) /* Jedi 2 */) 
+                match.IdVainqueur = match.Jedi1.ID;
+            else
+                match.IdVainqueur = match.Jedi2.ID;
         }
 
+        /// <summary>
+        /// Propriété d'accès aux matchs calculés par le gestionnaire du tournoi.
+        /// </summary>
         public List<MatchModel> Matchs
         {
             get { return m_matchs; }
