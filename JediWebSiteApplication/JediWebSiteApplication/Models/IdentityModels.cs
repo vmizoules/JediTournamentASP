3c42873d42c2f8ece041795e933d9eb42c0607fa
Rabérin Alexandre <kernelith@live.fr>
2016-03-17 23:11:58 +0100

    Si le get User Id était implémenté, cela serait certainement le moyen de récupérer le score de celui-ci via des infos personnalisées

--- JediWebSiteApplication/JediWebSiteApplication/Models/IdentityModels.cs
+++ JediWebSiteApplication/JediWebSiteApplication/Models/IdentityModels.cs
@@ -5,6 +5,10 @@ namespace JediWebSiteApplication.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int Score
+        {
+            get; set;
+        }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
